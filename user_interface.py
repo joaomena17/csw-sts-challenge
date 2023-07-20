d325affd221eb10afdf1ad7b8a2684664544cb9e
joaomena <joaomena@Ubuntu.myguest.virtualbox.org>
2023-07-20 10:53:36 +0100

    fix

--- user_interface.py
+++ user_interface.py
@@ -2,6 +2,6 @@ import streamlit as st
 import requests as r
          
 def get_sensor_data(sensor_id):
-    r.get("http://localhost:5000", sensor_id)
+    r.get(f"http://localhost:5000/sensors/{sensor_id}")
 
 get_sensor_data(1234)
