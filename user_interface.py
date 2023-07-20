995b5430fd0191c8319b3126a09d14cef7ff7efe
joaomena <joaomena@Ubuntu.myguest.virtualbox.org>
2023-07-20 10:46:01 +0100

    test front end and back end conn

--- user_interface.py
+++ user_interface.py
@@ -1,5 +1,13 @@
+from webbrowser import get
 import streamlit as st
+import requests as r
+from flask import request
 
 st.write("""
 # STS Challenge - IoT Project
 # """)
+
+def get_sensor_data(sensor_id):
+    r.get("http://localhost:5000", sensor_id)
+
+get_sensor_data(1234)
