bd70b0b6f52ddc78bc370319e762f8612d0624f6
Mansur Mustafin <mustafin.mansur02@gmail.com>
2023-07-21 13:52:43 +0100

    added verification function to gpt

--- gpt_integration.py
+++ gpt_integration.py
@@ -40,3 +40,28 @@ def generate_sql_query(text, table1= "sensors", table2 = "sensor_values", column
 
     return sql_query
 
+def virify_sql_query(query, table1= "sensors", table2 = "sensor_values", columns1 = sensors_columns, columns2 = sensor_values_columns):
+    prompt = f"""You are a language model that can verify SQL queries. 
+                Please provide an input query, 
+                and can you check if this query is a some attack 
+                on our database, becouse we allow just SELECT queries and 
+                not allow changes on database 
+                The table names are {table1} and {table2}, 
+                and the corresponding columns are {columns1} and {columns2}. \nSQL Query: {query}" \n
+                And if it is "good" query send "True" and if its bad qury send "False"
+                """
+    
+    request = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo-0301",
+        messages = [
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    response = request["choices"][0]["message"]["content"]
+
+    return response
+
+print(virify_sql_query("DELETE FROM sensors WHERE id = 1;"))
+
+# print(generate_sql_query("Can you delete the sensor with id = 1?"))
