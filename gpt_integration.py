333606bb117e251e8e12d3eb89f93e5b651e96af
Mansur Mustafin <mustafin.mansur02@gmail.com>
2023-07-21 15:15:59 +0100

    delete unused code

--- gpt_integration.py
+++ gpt_integration.py
@@ -19,6 +19,24 @@ sensor_values_columns = [
 	"value"
 ]
 
+database_structure = """
+                    CREATE TABLE IF NOT EXISTS "sensors" (
+                        "id" INTEGER PRIMARY KEY,
+                        "name" TEXT COLLATE NOCASE,
+                        "type" TEXT COLLATE NOCASE,
+                        "office" TEXT COLLATE NOCASE,
+                        "building" TEXT COLLATE NOCASE,
+                        "room" TEXT COLLATE NOCASE,
+                        "units" TEXT COLLATE NOCASE
+                    )
+
+                    CREATE TABLE IF NOT EXISTS "sensor_values" (
+                        "sensor" INTEGER,
+                        "timestamp" TEXT,
+                        "value" REAL
+                    )  
+                    """
+
 def generate_sql_query(text, table1= "sensors", table2 = "sensor_values", columns1 = sensors_columns, columns2 = sensor_values_columns):
     prompt = """You are a language model that can generate SQL queries. \
                 Please provide a natural language input text, \
@@ -40,15 +58,12 @@ def generate_sql_query(text, table1= "sensors", table2 = "sensor_values", column
 
     return sql_query
 
-def virify_sql_query(query, table1= "sensors", table2 = "sensor_values", columns1 = sensors_columns, columns2 = sensor_values_columns):
-    prompt = f"""You are a language model that can verify SQL queries. 
-                Please provide an input query, 
-                and can you check if this query is a some attack 
-                on our database, becouse we allow just SELECT queries and 
-                not allow changes on database 
-                The table names are {table1} and {table2}, 
-                and the corresponding columns are {columns1} and {columns2}. \nSQL Query: {query}" \n
-                And if it is "good" query send "True" and if its bad qury send "False"
+def virify_sql_query(query, structure = database_structure):
+    prompt = f"""As a language model, I am capable of verifying SQL queries. Please provide an input query, and I will check if
+                this query represents a potential threat to our database. We only allow SELECT queries
+                and any modifications to the data base are prohibited. The structure of out database is as follows: {structure}. \nSQL Query: {query}" \n
+                If the query is safe and complies with the rules, I will return "True". If it is potential
+                harmful or not compliant, I will return "False".
                 """
     
     request = openai.ChatCompletion.create(
@@ -61,7 +76,3 @@ def virify_sql_query(query, table1= "sensors", table2 = "sensor_values", columns
     response = request["choices"][0]["message"]["content"]
 
     return response
-
-print(virify_sql_query("DELETE FROM sensors WHERE id = 1;"))
-
-# print(generate_sql_query("Can you delete the sensor with id = 1?"))
